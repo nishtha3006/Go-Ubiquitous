0d108d6ba9d202e45e7a36e9499825ada09ed0d2
Nishtha A Boda <patelnishtha3006@gmail.com>
2016-12-15 19:37:26 +0530

    Added Listener service

---
+++ sunshinewearface/src/main/java/com/example/nishtha/sunshinewearface/WeatherListenerService.java
@@ -0,0 +1,33 @@
+package com.example.nishtha.sunshinewearface;
+
+/**
+ * Created by nishtha on 15/12/16.
+ */
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by Yifan on 6/11/16.
+ */
+public class WeatherListenerService extends WearableListenerService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
